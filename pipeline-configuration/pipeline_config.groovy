68a15edef1d697809bb1f60cbb3a9d121ed495c2
Mick Knutson <mickknutson@gmail.com>
2021-03-18 09:14:41 -0700

    more updates

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,4 +1,5 @@
 @merge libraries{
-    //gradle
-    sonarqube
+    //maven // references $GITHUB/jte-the-basics/libraries/maven/steps/build.groovy
+    //gradle // references $GITHUB/jte-the-basics/libraries/gradle/steps/build.groovy
+    sonarqube // references $GITHUB/jte-the-basics/libraries/sonarqube/steps/static_code_analysis.groovy
 }
